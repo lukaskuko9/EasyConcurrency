57df198252cdbcc7851c265a4618500d259cc4fb
lukaskuko9 <lukaskuko9@gmail.com>
2024-12-20 16:09:44 +0100

    better

--- Nuget/EasyConcurrency.Abstractions/Entities/LockableEntity/LockableConcurrentEntity.cs
+++ Nuget/EasyConcurrency.Abstractions/Entities/LockableEntity/LockableConcurrentEntity.cs
@@ -9,5 +9,5 @@ public abstract class LockableConcurrentEntity : LockableEntity, ConcurrentEntit
 {
     /// <inheritdoc />
     [Timestamp] 
-    public byte[] Version { get; init; } = [];
+    public byte[] Version { get; } = [];
 }
