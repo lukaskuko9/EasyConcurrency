60ee630bc0a5fdea47348e12583f19479ffc2d02
lukaskuko9 <lukaskuko9@gmail.com>
2025-12-14 20:04:18 +0100

    wrote tests for time lock entity extensions in EF

---
+++ Nuget/IntegrationTests/Tests/TimeLockEntityExtensionsTests.cs
@@ -0,0 +1,85 @@
+﻿using EasyConcurrency.Abstractions.Extensions;
+using EasyConcurrency.EntityFramework.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Stubs;
+using Xunit;
+
+namespace EasyConcurrency.IntegrationTests.Tests;
+
+public class TimeLockEntityExtensionsTests : DatabaseFixture
+{
+    [Fact]
+    public async Task WhereIsNotLocked_WorksAsIntended()
+    {
+        //Arrange
+        const int eachCollectionCount = 20;
+        var unlockedEntities = Enumerable.Range(0, eachCollectionCount)
+            .Select(i => new MyLockableEntity
+                {
+                    TestParameterGuid = Guid.NewGuid(),
+                    TestParameterString = i.ToString(),
+                    LockedUntil = null
+                }
+            )
+            .ToList();
+
+        var lockedEntities1Minute = Enumerable.Range(1 * eachCollectionCount, eachCollectionCount)
+            .Select(i => new MyLockableEntity
+                {
+                    TestParameterGuid = Guid.NewGuid(),
+                    TestParameterString = i.ToString(),
+                    LockedUntil = DateTimeOffset.UtcNow.AddMinutes(1)
+                }
+            )
+            .ToList();
+
+        var lockedEntities5Minutes = Enumerable.Range(2 * eachCollectionCount, eachCollectionCount)
+            .Select(i => new MyLockableEntity
+                {
+                    TestParameterGuid = Guid.NewGuid(),
+                    TestParameterString = i.ToString(),
+                    LockedUntil = DateTimeOffset.UtcNow.AddMinutes(5)
+                }
+            )
+            .ToList();
+        
+        Assert.True(lockedEntities1Minute.All(entity=>entity.IsLocked()));
+        Assert.True(lockedEntities5Minutes.All(entity=>entity.IsLocked()));
+        Assert.True(unlockedEntities.All(entity=>entity.IsNotLocked()));
+        await Context.MyLockableEntities.AddRangeAsync(unlockedEntities);
+        await Context.MyLockableEntities.AddRangeAsync(lockedEntities1Minute);
+        await Context.MyLockableEntities.AddRangeAsync(lockedEntities5Minutes);
+        await Context.SaveChangesAsync();
+        
+        //Act
+        var totalNumberOfEntities = await Context.MyLockableEntities.CountAsync();
+        var dbNotLockedWithImplicitNow = await Context.MyLockableEntities.WhereIsNotLocked().ToListAsync();
+        var dbNotLockedWithExplicitNow = await Context.MyLockableEntities.WhereIsNotLocked(DateTimeOffset.UtcNow).ToListAsync();
+        var dbNotLockedIn2Minutes = await Context.MyLockableEntities.WhereIsNotLocked(DateTimeOffset.UtcNow.AddMinutes(2)).ToListAsync();
+        //every item should be here as in 6 minutes every lock we set should have expired
+        var dbNotLockedIn6Minutes = await Context.MyLockableEntities.WhereIsNotLocked(DateTimeOffset.UtcNow.AddMinutes(6)).ToListAsync();
+        
+        //Assertions
+        
+        //Assert all collections unlocked set correctly
+        Assert.True(dbNotLockedWithImplicitNow.All(entity => entity.IsNotLocked()));
+        Assert.True(dbNotLockedWithExplicitNow.All(entity => entity.IsNotLocked()));
+        Assert.True(dbNotLockedIn2Minutes.All(entity => entity.IsNotLocked(DateTimeOffset.UtcNow.AddMinutes(2))));
+        Assert.True(dbNotLockedIn6Minutes.Count.Equals(totalNumberOfEntities));
+        
+        //Assert number of items in each collection is correct
+        
+        //entities that were not locked should have no lock at the moment
+        Assert.Equivalent(dbNotLockedWithImplicitNow, unlockedEntities);
+        
+        //should be same thing
+        Assert.Equivalent(dbNotLockedWithImplicitNow, dbNotLockedWithExplicitNow);
+        
+        //in 2 minutes, unlocked database entities should be entities we locked for 1 minute and entities we did not lock at all
+        Assert.Equivalent(dbNotLockedIn2Minutes, lockedEntities1Minute.Concat(unlockedEntities));
+        
+        //in 6 minutes, unlocked database entities should be entities we locked for 5 minutes + entities locked for 1 minute + entities we did not lock at all
+        Assert.Equivalent(dbNotLockedIn6Minutes, lockedEntities5Minutes.Concat(lockedEntities1Minute.Concat(unlockedEntities)));
+        
+    }
+}
